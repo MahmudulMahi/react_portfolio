a7d7b933a52aee24307c9d03d25875e78f50b8ff
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-01-31 23:56:23 +0600

    add project section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     { text: "Companies Work", count: "01" },
   ];
   return (
-    <section id="about" className="py-10 text-white">
+    <section id="about" className=" text-white">
       <div className="text-center mt-8">
         <h3 className="text-4xl font-semibold">
           About <span className="text-cyan-600">Me</span>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -29,10 +29,13 @@ const Skills = () => {
                  
                 </div>
               </div>
-              <p className="text-xl mt-3">Ex</p>
               <p className="text-xl mt-3">HTML 5</p>
+              <p className="text-xl mt-3">Advance</p>
+              
             </div>
             {/* --------- */}
+  
+            {/* -------- */}
             <div             
               className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
             >
@@ -47,102 +50,11 @@ const Skills = () => {
                  
                 </div>
               </div>
-              <p className="text-xl mt-3">Advance</p>
               <p className="text-xl mt-3">CSS 3</p>
+              <p className="text-xl mt-3">Advance</p>
+              
             </div>
             {/* ------- */}
-            <div             
-              className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
-            >
-              <div
-                style={{
-                  background: `conic-gradient(rgb(8,145,170) 86%,#ddd 86%)`,
-                }}
-                className="w-32 h-32 flex items-center justify-center rounded-full"
-              >
-                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center ">
-                  <ion-icon name="logo-javascript"></ion-icon>
-                 
-                </div>
-              </div>
-              <p className="text-xl mt-3">Advance</p>
-              <p className="text-xl mt-3">Javascript</p>
-            </div>
-            {/* --------- */}
-            <div             
-              className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
-            >
-              <div
-                style={{
-                  background: `conic-gradient(rgb(8,145,170) 86%,#ddd 86%)`,
-                }}
-                className="w-32 h-32 flex items-center justify-center rounded-full"
-              >
-                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center ">
-                  <ion-icon name="logo-react"></ion-icon>
-                 
-                </div>
-              </div>
-              <p className="text-xl mt-3">Advance</p>
-              <p className="text-xl mt-3">React</p>
-            </div>
-            {/* ---------- */}
-            <div             
-              className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
-            >
-              <div
-                style={{
-                  background: `conic-gradient(rgb(8,145,170) 86%,#ddd 86%)`,
-                }}
-                className="w-32 h-32 flex items-center justify-center rounded-full"
-              >
-                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center ">
-                  <ion-icon name="logo-nodejs"></ion-icon>
-                 
-                </div>
-              </div>
-              <p className="text-xl mt-3">Advance</p>
-              <p className="text-xl mt-3">Node Js</p>
-            </div>
-            {/* --------- */}
-            <div             
-              className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
-            >
-              <div
-                style={{
-                  background: `conic-gradient(rgb(8,145,170) 86%,#ddd 86%)`,
-                }}
-                className="w-32 h-32 flex items-center justify-center rounded-full"
-              >
-                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center ">
-             
-                  <img className="w-12 h-12" src={express} alt="" />
-               
-                 
-                </div>
-              </div>
-              <p className="text-xl mt-3">Advance</p>
-              <p className="text-xl mt-3">Express Js</p>
-            </div>
-            {/* ---------- */}
-            <div             
-              className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
-            >
-              <div
-                style={{
-                  background: `conic-gradient(rgb(8,145,170) 86%,#ddd 86%)`,
-                }}
-                className="w-32 h-32 flex items-center justify-center rounded-full"
-              >
-                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center ">
-                  <img className="w-12 h-12" src={mongo} alt="" />
-                 
-                </div>
-              </div>
-              <p className="text-xl mt-3">Advance</p>
-              <p className="text-xl mt-3">MongoDb</p>
-            </div>
-            {/* -------- */}
             <div             
               className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
             >
@@ -157,9 +69,109 @@ const Skills = () => {
                  
                 </div>
               </div>
-              <p className="text-xl mt-3">Advance</p>
               <p className="text-xl mt-3">Tailwind CSS</p>
+              <p className="text-xl mt-3">Advance</p>
+              
             </div>
+            {/* ------- */}
+            <div             
+              className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
+            >
+              <div
+                style={{
+                  background: `conic-gradient(rgb(8,145,170) 76%,#ddd 76%)`,
+                }}
+                className="w-32 h-32 flex items-center justify-center rounded-full"
+              >
+                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center ">
+                  <ion-icon name="logo-javascript"></ion-icon>
+                 
+                </div>
+              </div>
+              
+              <p className="text-xl mt-3">Javascript</p>
+              <p className="text-xl mt-3">Intermediate</p>
+            </div>
+            {/* --------- */}
+            <div             
+              className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
+            >
+              <div
+                style={{
+                  background: `conic-gradient(rgb(8,145,170) 70%,#ddd 70%)`,
+                }}
+                className="w-32 h-32 flex items-center justify-center rounded-full"
+              >
+                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center ">
+                  <ion-icon name="logo-react"></ion-icon>
+                 
+                </div>
+              </div>
+             
+              <p className="text-xl mt-3">React</p>
+              <p className="text-xl mt-3">Intermediate</p>
+            </div>
+            {/* ---------- */}
+            <div             
+              className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
+            >
+              <div
+                style={{
+                  background: `conic-gradient(rgb(8,145,170) 60%,#ddd 60%)`,
+                }}
+                className="w-32 h-32 flex items-center justify-center rounded-full"
+              >
+                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center ">
+                  <ion-icon name="logo-nodejs"></ion-icon>
+                 
+                </div>
+              </div>
+              
+              <p className="text-xl mt-3">Node Js</p>
+              <p className="text-xl mt-3">Expect</p>
+            </div>
+            {/* --------- */}
+            <div             
+              className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
+            >
+              <div
+                style={{
+                  background: `conic-gradient(rgb(8,145,170) 60%,#ddd 60%)`,
+                }}
+                className="w-32 h-32 flex items-center justify-center rounded-full"
+              >
+                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center ">
+             
+                  <img className="w-12 h-12" src={express} alt="" />
+               
+                 
+                </div>
+              </div>
+             
+              <p className="text-xl mt-3">Express Js</p>
+              <p className="text-xl mt-3">Expect</p>
+            </div>
+            {/* ---------- */}
+            <div             
+              className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
+            >
+              <div
+                style={{
+                  background: `conic-gradient(rgb(8,145,170) 60%,#ddd 60%)`,
+                }}
+                className="w-32 h-32 flex items-center justify-center rounded-full"
+              >
+                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center ">
+                  <img className="w-12 h-12" src={mongo} alt="" />
+                 
+                </div>
+              </div>
+           
+              <p className="text-xl mt-3">MongoDb</p>
+              <p className="text-xl mt-3">Expect</p>
+            </div>
+            {/* -------- */}
+    
         </div>
       </div>
     </section>
